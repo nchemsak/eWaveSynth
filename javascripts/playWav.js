4caa5de02b1c44750d749fce49511b8cea0271d9
Nick Chemsak <nchemsak@gmail.com>
2016-11-29 17:39:48 -0600

    keyboard add and audio freq

--- javascripts/playWav.js
+++ javascripts/playWav.js
@@ -1,9 +1,13 @@
 'use strict';
 var AudioContext;
+var audio_file;
+var audio_player;
+
 var context = new AudioContext();
 
 var audio = new Audio();
-audio.src = 'All%20the%20Same.wav';
+// audio.src = 'All%20the%20Same.wav';
+audio.src = '/audio/All%20the%20Same.wav';
 audio.controls = true;
 audio.volume = 1;
 document.body.appendChild(audio);
@@ -16,3 +20,12 @@ window.addEventListener('load', function(e) {
   source.connect(analyser);
   analyser.connect(context.destination);
 }, false);
+
+
+// audio_file.onchange = function() {
+//   var files = this.files;
+//   var file = URL.createObjectURL(files[0]);
+//   console.log("file: ", file);
+//   audio_player.src = file;
+//   audio_player.play();
+// };
